c9121addad78c8b70fbe11b7137c24aa24fe5283
Afton Downey and Oskar Radon <oskarradonkimball@gmail.com>
2016-01-21 10:08:58 -0800

    Add jQuery UI functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,24 @@
-var countUp = function(input, multiple) {
-  var nums = [];
-  for (var i = 1; i <= input; i++) {
+var nums = [];
+
+var countUp = function(countTo, multiple) {
+  for (var i = 1; i <= countTo; i++) {
     if ((i % multiple) === 0) { // if a number modulo multiple = 0
     nums.push(i);
     };
   };
-  
+
   return nums;
 };
+
+$(function(){
+  $("form#numbers").submit(function(event){
+    var countTo = parseInt($("input#num1").val());
+    var multiple = parseInt($("input#num2").val());
+
+    countUp(countTo, multiple);
+    // var resultString = nums.join(", ");
+    $("div#result").append(nums.join(", "));
+
+    event.preventDefault();
+  });
+});
