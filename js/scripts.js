4f5bf13cd5002c0294aa8f7c436519967ebdbd79
Afton Downey and Oskar Radon <aaike001@gmail.com>
2016-01-21 09:41:40 -0800

    Add second passing test for second multiplier parameter

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,10 @@
-var countUp = function(input) {
+var countUp = function(input, multiple) {
   var nums = [];
   for (var i = 1; i <= input; i++) {
+    if ((i % multiple) === 0) { // if a number modulo multiple = 0
     nums.push(i);
+    };
   };
+  
   return nums;
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,9 @@
 describe("countUp", function() {
   it("will count to a given number", function() {
-    expect(countUp(10)).to.eql([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    expect(countUp(10, 1)).to.eql([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  });
+
+  it("will count to a given number using a given multiple", function() {
+    expect(countUp(10, 2)).to.eql([2, 4, 6, 8, 10]);
   });
 });
