c0d81459745be32f50fedf24d9cbbb708c63ea62
Afton Downey and Oskar Radon <aaike001@gmail.com>
2016-01-21 10:51:45 -0800

    Add global var comment

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
-var nums = [];
+var nums = []; // THIS IS A GLOBAL VARIABLE (IMPORTANT)
 
 var countUp = function(countTo, multiple) {
   for (var i = 1; i <= countTo; i++) {
-    if ((i % multiple) === 0) { // if a number modulo multiple = 0
+    if ((i % multiple) === 0) {
     nums.push(i);
     };
   };
@@ -16,7 +16,7 @@ $(function(){
     var multiple = parseInt($("input#num2").val());
 
     countUp(countTo, multiple);
-    // var resultString = nums.join(", ");
+  
     $("div#result").append(nums.join(", "));
 
     event.preventDefault();
